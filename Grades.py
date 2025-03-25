13097dfa4c2532c7363b1764e8054b30f02df349
Jessica Iral Mendonca <jm2420@live.mdx.ac.uk>
2025-03-25 13:48:46 +0400

    Add files via upload

---
+++ Grades.py
@@ -0,0 +1,32 @@
+exam_one = int(input("Input exam grade one: "))
+
+exam_two = int(input("Input exam grade two: ")) # int function missing
+
+exam_three = int(input("Input exam grade three: ")) # not exam_3 because different value is used in list grade, also str is incorrect
+
+grades = [exam_one, exam_two, exam_three] # added , for separating values
+sum = 0
+for grade in grades: # correction instead of 'for grade in grade' we are calling from variable grades for grade values.
+  sum = sum + grade
+
+avg = sum / len(grades)
+
+if avg >= 90:
+    letter_grade = "A"
+elif avg >= 80 and avg < 90: # missing :
+    letter_grade = "B"
+elif avg >= 70 and avg < 80: # missing = after 70 ,incorrect range
+    letter_grade = "C" # ' is used instead of " 
+elif avg >= 60 and avg <70 : # < instead of >, incorrect range
+    letter_grade = "D"
+else:                   # correction else should be used in the end of if-elif statement, the for loop in the next line after letter_grade is removed since it is unncessary
+    letter_grade = "F"     
+    
+print(f"Exam marks = {sum}") # using Formatted string to extract the right variables in the print statement
+print(f"Average = {avg:.2f}")  # Limits to 2 decimal places
+print(f"Grade: {letter_grade}")
+
+if letter_grade == "F": # the symbol between letter grade is - instead underscore
+    print ("Student is failing.") # missing brackets after print function
+else:
+    print ("Student is passing.") 
