01a25192ca3fa350cf07533d7c014346f09dc9d5
Jessica Iral Mendonca <jm2420@live.mdx.ac.uk>
2025-03-25 13:57:30 +0400

    Add files via upload

---
+++ Pirate.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?") # mising "
+if greeting in ("Arrr!"): # incorrect bracket [
+	print("Go away, pirate.")
+else:            # if elif statement is wrong
+ print("Greetings, hater of pirates!") # space before print statement
